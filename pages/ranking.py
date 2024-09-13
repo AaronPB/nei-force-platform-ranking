32c42def7b1a63a2e3c8c9b1cc8b953ecafb79c3
AaronPB <arp629@gmail.com>
2024-09-13 18:18:38 +0200

    WIP Add dataframe ranking example format

--- pages/ranking.py
+++ pages/ranking.py
@@ -1,5 +1,35 @@
 import streamlit as st
+import pandas as pd
+
+# Just for graph testing
+import random
+
+
+def generateDataframe() -> pd.DataFrame:
+    df = pd.DataFrame(
+        {
+            "Nombre": ["Usuario1", "Usuario2", "Usuario3"],
+            "Equilibrio": [
+                [random.randint(-3, 3) for _ in range(100)] for _ in range(3)
+            ],
+            "Área elipse (cm2)": [1.23, 2.32, 2.45],
+            "Puntuación": [950, 870, 340],
+        }
+    )
+    df.index = df.index + 1
+    return df
 
 
 def ranking():
-    st.warning("En construcción!", icon=":material/construction:")
+    st.header(":material/scoreboard: Ranking del TOP 20")
+    st.write("¡Las 20 personas con mejor equilibrio al cerrar los ojos!")
+
+    st.dataframe(
+        data=generateDataframe(),
+        use_container_width=True,
+        column_config={
+            "Equilibrio": st.column_config.AreaChartColumn(
+                "Trayectoria del centro de presiones", y_min=-3, y_max=3
+            ),
+        },
+    )
