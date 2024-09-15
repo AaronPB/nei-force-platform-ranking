2dd81b5c276942856c483a743f9729da2f9a5bfb
AaronPB <arp629@gmail.com>
2024-09-15 14:51:47 +0200

    Add demo mode and results handling

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import yaml
 import time
+import random
+
+from src.managers import DataManager
+from src.handlers import SensorGroup
 
 from pages import ranking
 
@@ -46,6 +50,56 @@ def getAchievements(position: int, total: int, area: float) -> dict:
     return achievements
 
 
+def recordPlatforms(countdown):
+    with st.status("Iniciando grabación...") as status:
+        # TODO Connect sensors
+        for i in range(3, 0, -1):
+            countdown.metric("Iniciando", i)
+            time.sleep(1)
+        status.update(label="Grabando...")
+        for i in range(100, -1, -1):
+            seconds = i / 10
+            formatted_time = f"{int(seconds):02}.{int(i % 10)}"
+            countdown.metric("¡Grabando!", formatted_time)
+            # TODO Record from sensors
+            time.sleep(0.1)
+    countdown.empty()
+    status.update(label="Calculando...", state="running")
+    # TODO Get here results and build dataframe.
+    st.session_state.results = getData()
+    time.sleep(3)
+    status.update(label="¡Completado!", state="complete")
+
+
+def recordDemo(countdown):
+    with st.status("Iniciando grabación...") as status:
+        for i in range(3, 0, -1):
+            countdown.metric("Iniciando", i)
+            time.sleep(1)
+        status.update(label="Grabando...")
+        for i in range(40, -1, -1):
+            seconds = i / 10
+            formatted_time = f"{int(seconds):02}.{int(i % 10)}"
+            countdown.metric("¡Grabando!", formatted_time)
+            time.sleep(0.1)
+    countdown.empty()
+    status.update(label="¡Completado! Preparando resultados...", state="complete")
+    # Generate demo data
+    sensor_groups: list[SensorGroup] = st.session_state.sensor_mngr.getGroups()
+    for group in sensor_groups:
+        for sensor in group.getSensors().values():
+            # Just for test purposes.
+            # This simulated data is not correct because force variations affects other sensor measures.
+            if "Z" in sensor.getName():
+                sensor.values = [
+                    round(random.uniform(1.14e-04, 9.65e-05), 5) for _ in range(100)
+                ]
+            else:
+                sensor.values = [
+                    round(random.uniform(8.58e-05, 2.14e-05), 5) for _ in range(100)
+                ]
+
+
 def getData() -> pd.DataFrame:
     return pd.DataFrame(
         {
@@ -58,18 +112,22 @@ def getData() -> pd.DataFrame:
 
 
 def dashboard():
-    # Session variables
+    # Managers
+    if "data_mngr" not in st.session_state:
+        st.session_state.data_mngr = DataManager()
+    # Dashboard variables
     if "recording" not in st.session_state:
         st.session_state.recording = False
     if "results" not in st.session_state:
-        st.session_state.results = pd.DataFrame()
+        st.session_state.results = {}
+    if "get_balloons" not in st.session_state:
+        st.session_state.get_balloons = False
 
     # Page
     if st.session_state.get("btn_save", False):
         # TODO Merge data to ranked dataframe and backup csv file.
         # Reset states
         st.session_state.recording = False
-        st.session_state.results = pd.DataFrame()
         # Switch to ranking page
         st.switch_page(st.Page(ranking.ranking))
 
@@ -77,44 +135,56 @@ def dashboard():
         st.session_state.recording = True
     elif st.session_state.get("btn_rec_cancel", False):
         st.session_state.recording = False
-        st.session_state.results = pd.DataFrame()
 
     logger.debug(st.session_state.recording)
-    logger.debug(st.session_state.results.empty)
+    logger.debug(st.session_state.results)
 
     # Show results instead of recording button
-    if st.session_state.recording and not st.session_state.results.empty:
+    if st.session_state.recording and st.session_state.results:
         st.header("Resultados")
-        # TODO Check results to load multiple achievements
-        # TODO change vars to actual data
-        position = 670
-        total = 893
-        area = 5.32
+        position = st.session_state.results["position"]
+        total = st.session_state.results["total"]
+        area = st.session_state.results["area"]
         metric_col1, metric_col2 = st.columns(2)
         metric_col1.metric(
             "Posición ranking", f"Puesto nº {position}", f"De {total} personas", "off"
         )
-        metric_col2.metric("Área total trayectoria", f"{area} cm2")
+        metric_col2.metric("Área total trayectoria", f"{round(area, 2)} cm2")
 
         achievements = getAchievements(position, total, area)
         if achievements:
             for achievement in achievements.values():
                 st.warning(achievement[0], icon=achievement[1])
+        if not st.session_state.get_balloons:
             st.balloons()
-        else:
-            st.snow()
+            st.session_state.get_balloons = True
 
+        # Build COP graphs with plotly
+        st.plotly_chart(st.session_state.data_mngr.getFigure())
+
+        # Save options
+        st.data_editor(
+            data=st.session_state.results["dataframe"],
+            use_container_width=True,
+            disabled=("Trayectoria", "Área"),
+            column_config={
+                "Trayectoria": st.column_config.AreaChartColumn(
+                    "Trayectoria del centro de presiones", y_min=-30, y_max=30
+                ),
+            },
+            hide_index=True,
+        )
         col1, col2 = st.columns(2)
         col1.button(
             label="Guardar", key="btn_save", type="primary", use_container_width=True
         )
         col2.button(label="Cancelar", key="btn_rec_cancel", use_container_width=True)
-
-        st.header("Trayectorias del centro de presión")
-        # TODO Build COP graphs with plotly
         return
 
-    if not st.session_state.recording and st.session_state.results.empty:
+    st.session_state.results = {}
+    st.session_state.get_balloons = False
+
+    if not st.session_state.recording:
         st.info("Colócate en las plataformas", icon=":material/settings_accessibility:")
 
     _, countdown_col, _ = st.columns([2, 0.5, 2])
@@ -128,23 +198,30 @@ def dashboard():
         disabled=st.session_state.recording,
     )
 
+    btn_return = st.button(
+        label="Volver",
+        use_container_width=True,
+    )
+    if btn_return:
+        st.switch_page(st.Page(ranking.ranking))
+
     if btn_record_start:
-        with st.status("Iniciando grabación...") as status:
-            # TODO Connect sensors
-            for i in range(3, 0, -1):
-                countdown.metric("Iniciando", i)
-                time.sleep(1)
-            status.update(label="Grabando...")
-            for i in range(100, -1, -1):
-                seconds = i / 10
-                formatted_time = f"{int(seconds):02}.{int(i % 10)}"
-                countdown.metric("¡Grabando!", formatted_time)
-                # TODO Record from sensors
-                time.sleep(0.1)
-        countdown.empty()
-        status.update(label="Calculando...", state="running")
-        # TODO Get here results and build dataframe.
-        st.session_state.results = getData()
-        time.sleep(3)
-        status.update(label="¡Completado!", state="complete")
+        if (
+            not st.session_state.demo_enabled
+            and not st.session_state.test_mngr.getSensorConnected()
+        ):
+            logger.warning("No platforms connected!")
+            st.session_state.recording = False
+            st.switch_page(st.Page(dashboard))
+            return
+        if not st.session_state.demo_enabled:
+            logger.info("Starting platform record!")
+            recordPlatforms(countdown)
+        else:
+            logger.info("Starting demo!")
+            recordDemo(countdown)
+        # Get COP and area values
+        st.session_state.data_mngr.loadData(st.session_state.sensor_mngr.getGroups())
+        # TODO Save data in results
+        st.session_state.results = st.session_state.data_mngr.getResults()
         st.switch_page(st.Page(dashboard))
