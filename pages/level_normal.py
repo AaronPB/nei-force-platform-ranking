6a176d500dc298baf5a81aac9154d4f5d550f948
AaronPB <arp629@gmail.com>
2024-09-17 23:40:16 +0200

    WIP Add demo checks in level

--- pages/level_normal.py
+++ pages/level_normal.py
@@ -6,6 +6,30 @@ from pages import ranking
 from loguru import logger
 
 
+def startTest(test_info, figure):
+    pass
+
+
+def startDemo(test_info, figure):
+    with st.spinner("Ejecutando prueba"):
+        # Generate random path
+        path_length = 400
+        test_info.title("Quédate en el centro")
+        for i in range(60):
+            figure.plotly_chart(st.session_state.data_mngr.getDemoFramedFigure(i))
+            time.sleep(0.05)
+        test_info.title("¡Sigue el camino!")
+        for i in range(path_length):
+            figure.plotly_chart(st.session_state.data_mngr.getDemoFramedFigure(i + 60))
+            time.sleep(0.05)
+        test_info.title("¡Completado!")
+        for i in range(40):
+            figure.plotly_chart(
+                st.session_state.data_mngr.getDemoFramedFigure(i + 60 + path_length)
+            )
+            time.sleep(0.05)
+
+
 def level_normal():
     if "level_recorded" not in st.session_state:
         st.session_state.level_recorded = False
@@ -53,6 +77,8 @@ def level_normal():
         key="btn_start_test",
         type="primary",
         use_container_width=True,
+        disabled=not st.session_state.demo_enabled
+        and not st.session_state.platforms_connected,
     )
     btn_col2.button("Regenerar", use_container_width=True)
 
@@ -61,27 +87,22 @@ def level_normal():
     figure.plotly_chart(st.session_state.data_mngr.getCompleteFigure())
 
     if btn_start:
+        if (
+            not st.session_state.demo_enabled
+            and not st.session_state.platforms_connected
+        ):
+            logger.warning("No platforms connected!")
+            st.session_state.recording = False
+            st.switch_page(st.Page(level_normal))
+            return
         test_btns.empty()
+        if not st.session_state.demo_enabled:
+            logger.info("Starting platform record!")
+            startTest(test_info, figure)
+        else:
+            logger.info("Starting demo!")
+            startDemo(test_info, figure)
 
-        with st.spinner("Ejecutando prueba"):
-            # Generate random path
-            path_length = 400
-            test_info.title("Quédate en el centro")
-            for i in range(60):
-                figure.plotly_chart(st.session_state.data_mngr.getDemoFramedFigure(i))
-                time.sleep(0.05)
-            test_info.title("¡Sigue el camino!")
-            for i in range(path_length):
-                figure.plotly_chart(
-                    st.session_state.data_mngr.getDemoFramedFigure(i + 60)
-                )
-                time.sleep(0.05)
-            test_info.title("¡Completado!")
-            for i in range(40):
-                figure.plotly_chart(
-                    st.session_state.data_mngr.getDemoFramedFigure(i + 60 + path_length)
-                )
-                time.sleep(0.05)
         test_info.empty()
         st.session_state.level_recorded = True
         st.switch_page(st.Page(level_normal))
